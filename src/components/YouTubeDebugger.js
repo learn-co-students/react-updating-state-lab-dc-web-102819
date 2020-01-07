f2809013d9a6eaeb27274350bd0e85cab6de71b7
Kyle <kylepcole@gmail.com>
2020-01-06 21:10:24 -0500

    lab complete

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -2,31 +2,40 @@ import React, {Component} from 'react'
 
 class YouTubeDebugger extends Component {
 	
-	constructor (){
-		super()
-		this.state = {
-		  errors: [],
-		  user: null,
-		  settings: {
-		    bitrate: 8,
-		    video: { resolution: '1080p' }
-		  }
-		}
+	constructor() {
+	  super()
+	  this.state = {
+	    errors: [],
+	    user: null,
+	    settings: {
+	      bitrate: 8,
+	      video: {
+	        resolution: '1080p'
+	      }
+	    }
+	  }
 	}
 
 	bitrateClick = () => {
-		this.setState = {
-			bitrate: 12
-		}
+		this.setState({
+	      settings: {
+	        ...this.state.settings,
+	        bitrate: 12
+	      }
+	    })
 	}
 
 
 	resolutionClick = () => {
-		this.setState = {
-			settings: Object.assign({}, this.state.settings.video, {
-				resolution: '720p'
-			})
-		}
+		this.setState({
+	      settings: {
+	        ...this.state.settings,
+	        video: {
+	          ...this.state.settings.video,
+	          resolution: '720p'
+	        }
+	      }
+	  })
 	}
 
 
